7e0f8253ddd030dfa647a847a1ab23469b6dbb5d
Daniel Suarez <alu0101040882@ull.edu.es>
2018-11-13 21:21:14 +0000

    Expectativas clasificacion de sal Creadas

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe Etiqueta_nut do
 		@dll.insert(Etiqueta_nut.new("Arroz",10,10,10,10,10,10,10,10,10,10,10,10,10))
 		@dll.insert(Etiqueta_nut.new("Cereales",10,10,10,10,10,10,10,10,10,10,10,10,10))
 	end
+
+describe "# Funciona correctamente" do
+	it "Clasifica sal"
+
+		@dll.clasify_salt
+		aux = @head
+		
+		while aux != nil do
+			expect(aux.value).to be <= aux.next.value
+			aux = aux.next
+		end
+	end
+end
+
 end
